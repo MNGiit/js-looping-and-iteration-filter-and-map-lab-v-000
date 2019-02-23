c8ca67af7ff1f2359198594ee178d19a95dbc33b
Mohammed Noori <kmanoorib@gmail.com>
2019-02-23 22:08:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution here:
 function driversWithRevenueOver(drivers, revenueOver) {
-  return drivers.map(function(driver) {return driver.revenue > revenueOver;});
+  return drivers.map(function(driver) {if (driver.revenue > revenueOver){return driver}});
 }
 
 
