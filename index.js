b803a1d11580f1cdc680dbf999ecec0d6bc91cbb
Mohammed Noori <kmanoorib@gmail.com>
2019-02-23 22:24:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,27 @@ function driversWithRevenueOver(drivers, revenue) {
 function driverNamesWithRevenueOver(drivers, revenue) {
   return driversWithRevenueOver(drivers, revenue).map(function (driver) {return driver.name;});
 }
+
+function exactMatch() {
+  
+}
+
+
+
+
+
+exactMatch()
+returns an array of all matching drivers ‣
+ReferenceError: exactMatch is not defined
+    at Context.<anonymous> (test/indexTest.js:55:7)expect(exactMatch(extendedDrivers, { name: 'Sally' })).to.eql([
+  { name: 'Sally', revenue: 400 },
+  { name: 'Sally', revenue: 200 }
+]);
+expect(exactMatch(extendedDrivers, { revenue: 200 })).to.eql([
+  { name: 'Annette', revenue: 200 },
+  { name: 'Sally',   revenue: 200 }
+]);
+returns an empty array if there is no match ‣
+ReferenceError: exactMatch is not defined
+    at Context.<anonymous> (test/indexTest.js:67:7)expect(exactMatch(extendedDrivers, { revenue: 500 })).to.eql([]);
+expect(exactMatch(extendedDrivers, { name: 'Alex' })).to.eql([]);
