f8ba716d753c2c1a229dc39e7e42cf2b30546341
Mohammed Noori <kmanoorib@gmail.com>
2019-02-23 22:04:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 // Code your solution here:
 function driversWithRevenueOver(drivers, revenueOver) {
-  
+  drivers.map
 }
 
+returns an array of all matching drivers ‣
+AssertionError: expected undefined to deeply equal [ { name: 'Sally', revenue: 400 } ]
+    at Context.<anonymous> (test/indexTest.js:28:55)expect(driversWithRevenueOver(drivers, 250)).to.eql([
+  { name: 'Sally', revenue: 400 }
+]);
+expect(driversWithRevenueOver(drivers, 199)).to.eql([
+  { name: 'Sally',   revenue: 400 },
+  { name: 'Annette', revenue: 200 }
+]);
+returns an empty array if there is no match ‣
+AssertionError: expected undefined to deeply equal []
+    at Context.<anonymous> (test/indexTest.js:39:55)expect(driversWithRevenueOver(drivers, 500)).to.eql([]);
