846f99e7deb6b28c193a5064241982487a00f463
Mohammed Noori <kmanoorib@gmail.com>
2019-02-23 22:20:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ function driversWithRevenue(drivers, revenue) {
   return drivers.filter(function (driver) {return driver.revenue > revenue;});
 }
 
-
-
+function driverNamesWithRevenueOver(drivers, revenue) {
+  return driversWithRevenue(drivers, revenue).map(function (driver) {return driver.name;});
+}
